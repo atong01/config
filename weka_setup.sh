65b58facff0c1bb93cf795c5125c503a2d96fe18
Alexander Tong <alexanderytong@gmail.com>
2015-02-09 22:20:51 -0500

    modified weka_setup

--- weka_setup.sh
+++ weka_setup.sh
@@ -1,7 +1,11 @@
 
+
+
 #on "linux", assuming your shell is bash, you should "source" this file.
 # otherwise, adjust to set environment variables CLASSPATH and WEKADATA 
 
+#modified by Alex Tong for adaptation to cshell compatability.
+#CAUTION: Overwrites classpath. If classpath already exists, then add a :$CLASSPATH to the end of the first statement
 setenv CLASSPATH /r/aiml/ml-software/weka-3-6-11/weka.jar
 setenv WEKADATA /r/aiml/ml-software/weka-3-6-11/data/
 
