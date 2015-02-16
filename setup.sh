1c89381871a94c810a330682d5aac25237d38cea
atong01 <alexanderytong@gmail.com>
2015-02-16 17:01:36 -0500

    added .bashrc prompt change

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 #setup.sh
-#Written By Alex Tong January 28, 2015
+#Created By Alex Tong January 28, 2015
+#Modified By Alex Tong February 16, 2015
 #This script runs the config package to set up a remote linux machine
 
 echo "Running ssh-config.sh"
 ./ssh-config.sh
 echo "copying .vimrc"
 cp .vimrc ~
+echo "changing bash prompt"
+echo "#bash prompt change by Alex Tong" >> ~/.bashrc
+echo "PS1=\h:\w $ " >> ~/.bashrc
+
 echo "setup complete"
 
