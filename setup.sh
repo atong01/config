20c595c3b03439d9f9deecb5ab80b228ffd6adc9
atong01 <alexanderytong@gmail.com>
2015-03-04 15:21:44 -0500

    change setup file for shell dependent prompt change

--- setup.sh
+++ setup.sh
@@ -9,9 +9,19 @@ echo "Running ssh-config.sh"
 ./ssh-config.sh
 echo "copying .vimrc"
 cp .vimrc ~
-echo "changing bash prompt"
-echo "#bash prompt change by Alex Tong" >> ~/.bashrc
-echo "\"PS1=\h:\w $ \"" >> ~/.bashrc
+echo "changing prompt"
+if [ $SHELL == /bin/tcsh ]; then
+    echo tcsh
+    echo "#tcsh prompt change by Alex Tong" >> ~/.bashrc
+    echo "\"PS1=\h:\w $ \"" >> ~/.bashrc
+elif [ $SHELL == /bin/bash ]; then
+    echo bash
+    echo "#bash prompt change by Alex Tong" >> ~/.bashrc
+    echo "\"PS1=\h:\w $ \"" >> ~/.bashrc
+else
+    echo shell not found
+fi
+
 echo "setup complete"
 
 ###############################################################################
