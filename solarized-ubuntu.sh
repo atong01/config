283b5420d50185ef36d380602c32531a4af17e67
atong01 <alexanderytong@gmail.com>
2017-02-13 01:48:38 -0500

    added source

--- solarized-ubuntu.sh
+++ solarized-ubuntu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # This script was tested on ubuntu 16.04 (prodigy) Feb 17
+#
+# http://www.webupd8.org/2011/04/solarized-must-have-color-paletter-for.html
 
 wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
 mv dircolos.ansi-dark ~/.dircolors
